6316d90462bf33004213bf2e4cc2cee11568013c
ramtin <ramtinak@live.com>
2021-05-06 15:01:27 +0430

    Add full code

--- InstagramRegistrationSample/Program.cs
+++ InstagramRegistrationSample/Program.cs
@@ -68,10 +68,104 @@ namespace InstagramRegistrationSample
 
         static async Task MainAsync()
         {
-        
+            var username = "username";
+            var password = "password";
+            var email = "abc@email.com";
+            var firstName = "my name"; // optional, but don't pass null, put string.Empty or ""
+
+
+            var userSession = new UserSessionData
+            {
+                UserName = username,
+                Password = password
+            };
+
+            var delay = RequestDelay.FromSeconds(2, 4);
+            InstaApi = InstaApiBuilder.CreateBuilder()
+                .SetUser(userSession)
+                .UseLogger(new DebugLogger(LogLevel.All))
+                .SetRequestDelay(delay)
+                // session handler, set a file path to save/load your state/session data
+                .SetSessionHandler(new FileSessionHandler { FilePath = username + ".state" })
+                .Build();
+
+            // Set api version to v180 [ PLEASE READ THE WARNING SECTION IN THIS PAGE! ]
+            InstaApi.SetApiVersion(InstaApiVersionType.Version180);
+
+            // no need to do this but I just wanted to show you
+            InstaApi.RegistrationService.Birthday = InstaApi.RegistrationService.GenerateRandomBirthday();
+
+
+            email = email.ToLower();
+            username = username.ToLower();
+
+            await InstaApi.RegistrationService.FirstLauncherSyncAsync();
+            await InstaApi.RegistrationService.FirstQeSyncAsync();
+
+            var checkEmailResult = await InstaApi.RegistrationService.CheckEmailAsync(email);
+            // if email address is available to create
+            if (checkEmailResult.Succeeded && (checkEmailResult.Value?.Available ?? false))
+            {
+                await Delay(1.5);
+                var signupConsent = await InstaApi.RegistrationService.GetSignupConsentConfigAsync();
+                await Delay(1.5);
+                await InstaApi.RegistrationService.SendRegistrationVerifyEmailAsync(email);
+
+                // check registration code that instagram sent it to your email:
+                var verificationCode = "";
+                await Delay(3.5);
+                var checkRegistrationConfirmationResult = await InstaApi.RegistrationService
+                    .CheckRegistrationConfirmationCodeAsync(email, verificationCode);
+
+                if (checkRegistrationConfirmationResult.Succeeded)
+                {
+
+                    await InstaApi.RegistrationService.GetSiFetchHeadersAsync();
+
+                    await Delay(1.5);
+                    await InstaApi.RegistrationService.GetUsernameSuggestionsAsync(firstName, email);
+
+                    await Delay(1.5);
+
+                    if (signupConsent.Value?.AgeRequired ?? false)
+                        await InstaApi.RegistrationService.CheckAgeEligibilityAsync();
+
+                    await Delay(2.5);
+                    await InstaApi.RegistrationService.GetOnboardingStepsAsync(InstaOnboardingProgressState.Prefetch);
+
+                    await InstaApi.RegistrationService.NewUserFlowBeginsConsentAsync();
+                    await InstaApi.RegistrationService.NewUserFlowBeginsConsentAsync();
+
+                    await Delay(2.5);
+                    var checkUsernameResult = await InstaApi.RegistrationService.CheckUsernameAsync(username);
+                    if (checkUsernameResult.Succeeded && (checkUsernameResult.Value?.Available ?? false))
+                    {
+                        var signupCode = checkRegistrationConfirmationResult.Value.SignupCode;
+                        var createAccount =
+                            await InstaApi.RegistrationService.CreateNewAccountWithEmailAsync(email, username, password, firstName, signupCode);
+                        if (!createAccount.Succeeded)
+                        {
+                            Console.WriteLine(createAccount.Info.Message);
+                            return;
+                        }
+
+                        await InstaApi.RegistrationService.GetMultipleAccountsFamilyAsync();
+                        await InstaApi.RegistrationService.GetZrTokenResultAsync();
+                        await InstaApi.RegistrationService.LauncherSyncAsync();
+                        await InstaApi.RegistrationService.QeSyncAsync();
+                        await InstaApi.RegistrationService.NuxNewAccountSeenAsync();
+                        await InstaApi.RegistrationService.GetOnboardingStepsAsync(InstaOnboardingProgressState.Start);
+                        await InstaApi.RegistrationService.GetContactPointPrefillAsync();
+                        await InstaApi.RegistrationService.GetOnboardingStepsAsync(InstaOnboardingProgressState.Finish);
+
+
+                        // now we can save our session to local:
+                        InstaApi.SessionHandler?.Save();
+
+                    }
+                }
+            }
         }
-
-
 
         static async Task Delay(double seconds)
         {
